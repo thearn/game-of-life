1cff66679fa9ede8bfad851442a01e430b9f8889
Alex Greason <quantum935@yahoo.com>
2018-02-01 19:14:13 -0600

    Revert convolution change
    The other one is faster, but it requires using a merely (3,3) kernel rather than one the size of the full state, so it isn't no-changes compatible, and it requires scipy so the travis build fails

--- lib/lib.py
+++ lib/lib.py
@@ -1,10 +1,14 @@
 from numpy.fft import fft2, ifft2, fftshift
 import numpy as np
-import scipy.signal as signal
-
+	
 def fft_convolve2d(x,y):
-    """
-    2D convolution, using FFT
-    """
-    cc = signal.convolve2d(x, y, mode="same", boundary="wrap")
-    return cc
+	"""
+	2D convolution, using FFT
+	"""
+	fr = fft2(x)
+	fr2 = fft2(np.flipud(np.fliplr(y)))
+	m,n = fr.shape
+	cc = np.real(ifft2(fr*fr2))
+	cc = np.roll(cc, - int(m / 2) + 1, axis=0)
+	cc = np.roll(cc, - int(n / 2) + 1, axis=1)
+	return cc
